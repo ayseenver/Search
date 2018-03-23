fdf5f3a326eaae8923aacbfeffbb2e173f4d050f
Ayse Enver <ayse.enver@city.ac.uk>
2018-03-23 13:43:35 +0000

    first commit

---
+++ src/search/Search.java
@@ -0,0 +1,195 @@
+package search;
+
+public class Search {
+
+    public int longestWord(StringList a) {
+        int pos = 0;
+        int i = 1;
+        String longest = a.get(pos);
+        String current;
+        
+        /*
+        Invariant: 1 ≤ i ≤ n and longest is longest word of a[0] ... a[i-1]
+        and pos is the position of longest.
+        */
+        
+        while (i < a.size()) {
+            current = a.get(i);
+            if (current.length() > longest.length()) {
+                pos = i;
+                longest = current;
+            }
+            i++;
+        }
+        return pos;
+    }
+
+    public int countUnique(StringList a) {
+        String prev = a.get(0);
+        String current;
+        int unique = 1;
+        int i = 1;
+        
+        /*
+        Invariant: 0 ≤ i ≤ a.size() and unique = number of unique elements
+        from a[0] ... a[i-1]
+        */
+        
+        while (i < a.size()) {
+            current = a.get(i);
+            if (!current.equals(prev)) {
+                unique++;
+            }
+            i++;
+            prev = current;
+        }
+        return unique;
+    }
+
+    public String mostFrequent(StringList a) {
+        int i = 1;
+        int freq = 1;
+        int mostFrequentIndex = 0;
+        int mostFrequent = 0;
+
+        String popular;
+        String current;
+        String prev = a.get(i - 1);
+        
+        while (i < a.size()) {
+            current = a.get(i);
+            if (current.equals(prev)) {
+                freq++;
+            } else {
+                if (freq > mostFrequent) {
+                    mostFrequent = freq;
+                    mostFrequentIndex = i - 1;
+                }
+                freq = 1;
+            }
+            prev = current;
+            i++;
+        }
+        popular = a.get(mostFrequentIndex);
+        return popular;
+    }
+
+    public int findElement(StringList a, String k) {
+        int low = 0;
+        int high = a.size() - 1;
+        int mid;
+        String current;
+        
+        /*
+        invariant: 0 ≤ low ≤ high+1 ≤ a.size() and a[0..low-1] < k < a[high+1..n-1]
+        */
+        
+        while (low <= high) {
+            mid = (high + low) / 2;
+            current = a.get(mid);
+            if (current.equals(k)) {
+                return mid;
+            } else if (current.compareTo(k) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int countGreaterOrEqual(StringList a, String k) {
+        int low = 0;
+        int high = a.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (a.get(mid).compareTo(k) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return a.size() - low;
+    }
+
+    public int countGreater(StringList a, String k) {
+        int low = 0;
+        int high = a.size() - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (a.get(mid).compareTo(k) > 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return a.size() - low;
+    }
+
+    public int countBetween(StringList a, String k1, String k2) {
+        int low = 0;
+        int high = a.size() - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (a.get(mid).compareTo(k1) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        int k1Pos = low;
+
+        low = 0;
+        high = a.size() - 1;
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (a.get(mid).compareTo(k2) > 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int k2Pos = low;
+
+        return k2Pos - k1Pos;
+    }
+
+    public int countMatches(StringList a, String prefix, String substring) {
+        int matches = 0;
+
+        int low = 0;
+        int high = a.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (a.get(mid).compareTo(prefix) < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        boolean flag = true;
+        String current;
+        
+        while(flag){
+            current = a.get(low);
+            if(current.startsWith(prefix)){
+                if (current.contains(substring)){
+                    matches++;
+                }
+            }else{
+                flag = false;
+            }
+            low++;
+        }
+        
+        return matches;
+    }
+}
---
+++ src/search/SearchTest.java
@@ -0,0 +1,54 @@
+package search;
+
+import java.io.IOException;
+
+/**
+ * Test program for the Search class.
+ */
+public class SearchTest {
+
+    /**
+     * Test program for the Search class. Put whatever tests you like in the
+     * body of the method.
+     *
+     * @param args the command line arguments
+     * @throws java.io.IOException of error reading the input
+     */
+    public static void main(String[] args) throws IOException {
+        // Don't change this line
+        final Search search = new Search();
+
+        // You can set this to any of the text files in the data folder
+        final FileStrings strings = new FileStrings("data/large.txt");
+
+        // add your tests here
+        System.out.println(search.longestWord(strings));
+        System.out.println();
+
+        System.out.println(search.countUnique(strings));
+        System.out.println();
+
+        System.out.println("Result: " + search.mostFrequent(strings));
+        System.out.println();
+
+        System.out.println(search.findElement(strings, "the"));
+        System.out.println(search.findElement(strings, "ziggurat"));
+        System.out.println();
+
+        System.out.println(search.countGreaterOrEqual(strings, "its"));
+        System.out.println(search.countGreaterOrEqual(strings, "orange"));
+        System.out.println();
+
+        System.out.println(search.countGreater(strings, "guardian"));
+        System.out.println(search.countGreater(strings, "them"));
+        System.out.println();
+
+        System.out.println(search.countBetween(strings, "first", "they"));
+        System.out.println(search.countBetween(strings, "guardian", "the"));
+        System.out.println();
+
+        System.out.println(search.countMatches(strings, "on", "l"));
+        System.out.println(search.countMatches(strings, "the", "em"));
+    }
+
+}
